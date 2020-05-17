2c2939540141305600f4ab6b0c246733a9f0cab4
zjf <zjffun@gmail.com>
2020-05-17 18:38:14 +0800

    save

--- reg2nfa.js
+++ reg2nfa.js
@@ -1,3 +1,5 @@
+const allNodes = [];
+
 function getRegReversePolishNotation(reg) {
   let i = 0,
     stack = [],
@@ -40,12 +42,16 @@ function regRPN2NFA(reg) {
       case "a":
         state1 = [[], [], [], num++];
         state2 = [[], [], [], num++];
+        allNodes.push(state1);
+        allNodes.push(state2);
         state1[0].push(state2);
         stack.push([state1, state2]);
         break;
       case "b":
         state1 = [[], [], [], num++];
         state2 = [[], [], [], num++];
+        allNodes.push(state1);
+        allNodes.push(state2);
         state1[1].push(state2);
         stack.push([state1, state2]);
         break;
@@ -61,6 +67,7 @@ function regRPN2NFA(reg) {
         break;
       case "*":
         state1 = [[], [], [], num++];
+        allNodes.push(state1);
         statePeer1 = stack.pop();
         statePeer1[0][2].push(state1);
         statePeer1[1][2].push(state1);
@@ -70,6 +77,8 @@ function regRPN2NFA(reg) {
       case "|":
         state1 = [[], [], [], num++];
         state2 = [[], [], [], num++];
+        allNodes.push(state1);
+        allNodes.push(state2);
         statePeer1 = stack.pop();
         statePeer2 = stack.pop();
         state1[2].push(statePeer1[0]);
@@ -82,4 +91,48 @@ function regRPN2NFA(reg) {
     i++;
   }
   return stack;
+}
+
+function rmEpsilon(nfa) {
+  const receiving = nfa[1];
+  const remainNodes = new Set(allNodes);
+  function epsilonNode(node) {
+    if (node === receiving || !node[2].length || !remainNodes.has(node)) {
+      remainNodes.delete(node);
+      return node;
+    }
+
+    const nodes = [];
+    node[2].forEach((d) => {
+      const enode = epsilonNode(d);
+      nodes.push(enode);
+    });
+
+    node[0] = [
+      ...new Set([
+        ...node[0],
+        ...nodes.reduce((res, cur) => {
+          return res.concat(cur[0]);
+        }, []),
+      ]),
+    ];
+    node[1] = [
+      ...new Set([
+        ...node[1],
+        ...nodes.reduce((res, cur) => {
+          return res.concat(cur[1]);
+        }, []),
+      ]),
+    ];
+
+    remainNodes.delete(node);
+    return node;
+  }
+
+  while (remainNodes.size) {
+    const val = remainNodes.values().next().value;
+    epsilonNode(val);
+  }
+
+  return nfa;
 }
